fa78b1e738d2199d45a21a7c167048e4a5376a64
Stailly <laerta-ola@mail.ru>
2020-04-13 23:24:14 +0300

    home work 5

--- src/hw5/HomeWork.java
+++ src/hw5/HomeWork.java
@@ -6,33 +6,41 @@ public class HomeWork {
     static final int SIZE = 10000000;
     static final int HALF = SIZE / 2;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         float[] arr = new float[SIZE];
         float[] newArr = new float[SIZE];
         Arrays.fill(arr, 1);
-       useOneThread(arr);
-       System.out.println(" ");
+        useOneThread(arr);
+        System.out.println("");
         Arrays.fill(newArr, 1);
         useTwoThread(newArr);
+        System.out.println(Arrays.equals(arr, newArr));
     }
 
     public static void useOneThread(float[] floats) {
         long a = System.currentTimeMillis();
-        changeValues(floats);
+        changeValues(floats, 0);
         System.currentTimeMillis();
-               System.out.println(System.currentTimeMillis() - a);
+        System.out.println(System.currentTimeMillis() - a);
     }
 
 
-    public static void useTwoThread (float[] floats){
+    public static void useTwoThread(float[] floats) throws InterruptedException {
         long a = System.currentTimeMillis();
-        float[]arr1 = new float[HALF];
-        float[]arr2 = new float[HALF];
+        float[] arr1 = new float[HALF];
+        float[] arr2 = new float[HALF];
         System.arraycopy(floats, 0, arr1, 0, HALF);
         System.arraycopy(floats, HALF, arr2, 0, HALF);
 
-        new Thread(() -> changeValues(arr1)).start();
-        new Thread(() -> changeValues(arr2)).start();
+        Thread thread1 = new Thread(() -> changeValues(arr1, 0));
+        Thread thread2 = new Thread(() -> changeValues(arr2, HALF));
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
 
         System.arraycopy(arr1, 0, floats, 0, HALF);
         System.arraycopy(arr2, 0, floats, HALF, HALF);
@@ -41,11 +49,9 @@ public class HomeWork {
         System.out.println(System.currentTimeMillis() - a);
     }
 
-    private static void changeValues(float[] floats) {
-        for (int i = 0; i < floats.length; i++) {
-            floats[i] = (float) (floats[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    private static void changeValues(float[] floats, int newI) {
+        for (int i = 0; i < floats.length; i++, newI++) {
+            floats[i] = (float) (floats[i] * Math.sin(0.2f + newI / 5.0) * Math.cos(0.2f + newI / 5.0) * Math.cos(0.4f + newI / 2.0));
         }
     }
-
-
 }
